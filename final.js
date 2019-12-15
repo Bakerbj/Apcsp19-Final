54a19e62235de500c8bda1593ab946e06292f700
Brennan Baker <bakerbj26@gmail.com>
2019-12-14 23:50:05 -0700

    Added more

--- final.js
+++ final.js
@@ -18,6 +18,7 @@ function ready () {
     mids.forEach(c => {
         c.classList.remove("hide");
     })
+    back.classList.add("hide");
     // thirds.forEach(c => {
     //     c.classList.add("hide");
     // });
@@ -73,26 +74,48 @@ function ready () {
     
    
     document.basics.onsubmit = function(e) {
+        // if (name == ""|| place == ""|| weath == "" || sid == "" || relate == "") {
+        //     return "please fill out all info"
+        // }
         e.preventDefault();
+        back.classList.remove("hide");
         mids.forEach(c => {
             c.classList.add("hide");
         });
         let bet = ""
         let noun = ""
+        let chars = []
+        if (gen.value == "mystery") {
+            chars.push("human", "detective", "murderer")
+        } else if (gen.value == "romance") {
+            chars.push("human", "lover", "love interest")
+        } else if (gen.value == "sci-fi") {
+            chars.push("alien", "robot", "stormtrooper")
+        } else if (gen.value == "fantasy") {
+            chars.push("Ogre", "Fairy", "member of the royal family")
+        } else if (gen.value == "drama") {
+            chars.push("dramatic", "neurosurgeon", "unemployed person")
+        } else if (gen.value == "math") {
+            chars.push("nerd", "geek", "teacher")
+        } else if (gen.value == "short_story") {
+            chars.push("random character", "human", "marshmallow")
+        }
+        
+        let char = chars[Math.floor(Math.random() * 3)];
         //let subject = ""
-        let snouns = ["house ", "car ", "brownie ", "computer ", "cookie "];
-        let pnouns = ["houses ", "cars ", "brownies ", "computers ", "cookies "];
-        let verbs = ["erasing ", "watching ", "eating ", "making ", "stabbing "]
+        let snouns = ["house ", "car ", "brownie ", "computer ", "cookie ", "rattlesnake ", "village ", "walnut ", "person "];
+        let pnouns = ["houses ", "cars ", "brownies ", "computers ", "cookies ", "rattlesnakes ", "villages ", "walnuts ", "people "];
+        let verbs = ["erasing ", "watching ", "eating ", "making ", "stabbing ", "jumping on ", "throwing ", "playing with ", "thinking about ", "laughing at ", "yelling at ", "writing about ", "insulting "]
         let sorpn = [Math.floor(Math.random() * 2)];
         if (sorpn == 0) {
-            noun = snouns[Math.floor(Math.random() * 5)];
+            noun = snouns[Math.floor(Math.random() * 8)];
             bet = "a ";
         }
         if (sorpn == 1) {
-            noun = pnouns[Math.floor(Math.random() * 5)];
+            noun = pnouns[Math.floor(Math.random() * 8)];
             bet = "some ";
         }
-        let verb = verbs[Math.floor(Math.random() * 5)];
+        let verb = verbs[Math.floor(Math.random() * 13)];
         let norp = [Math.floor(Math.random() * 2)];
         if (norp == 1){
             subject = name.value;
@@ -101,7 +124,7 @@ function ready () {
             subject = "they";
         }
 
-        sentsf = name.value + " is " + verb + bet + noun + "in " + place.value + "."
+        sentsf = name.value + " the " + char + " was " + verb + bet + noun + "in " + place.value + "."
         //sentsn = subject + pv + 
         let el = document.createElement('p');
             el.innerHTML = sentsf;
@@ -109,6 +132,12 @@ function ready () {
         empty.append(el);
 
     };
+    back.onclick = function () {
+    back.classList.add("hide");
+        mids.forEach(c => {
+            c.classList.remove("hide");
+        });
+    }
 };
 
 document.addEventListener("DOMContentLoaded", ready);
